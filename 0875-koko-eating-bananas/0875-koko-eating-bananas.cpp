fc14011e5eaf12567b3aa39080fc65e8c48e34f6
Muhammed. Suhail <mdsuhail63831@gmail.com>
2025-07-25 10:23:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n = piles.size();
+        int start = 0, end = 0, mid , ans;
+        long long sum =0;
+        //find start and end
+        for(int i = 0; i<n;i++){
+            sum = sum+ piles[i];
+            end = max(end,piles[i]);
+        }
+
+        start = sum/h;
+
+        while(start<=end){
+            mid = start+(end-start)/2;
+            int total_times = 0;
+            for(int i = 0;i<n;i++){
+                total_times = total_times+piles[i]/mid;
+                if(piles[i]%mid)
+                total_times++;
+            }
+            if(total_times<=h)
+            {
+                ans = mid;
+                end = mid-1;
+            }
+            else
+            start = mid+1;
+        }
+
+        return  ans;
+        
+    }
+};
