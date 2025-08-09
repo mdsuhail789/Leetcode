660a170ddde37e265fd8bb77ad613a63f42d594f
Muhammed. Suhail <mdsuhail63831@gmail.com>
2025-08-09 18:55:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0018-4sum/0018-4sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+
+        int n = nums.size();
+        vector<vector<int>>ans;
+
+        // first element 
+        for(int n =0; i<n;i++){
+            
+        }
+        if(i>0 && nums[i] ==nums[i-1]);
+
+        // second element 
+        for(int j = i+1; i<n; i++){
+
+        }
+        for
+        
+    }
+};
