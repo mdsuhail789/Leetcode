74093ee1399d6dcd4cf4ac8e96699b8358360e39
Muhammed. Suhail <mdsuhail63831@gmail.com>
2026-07-17 22:58:07 +0530

    Time: 0 ms (100%), Space: 53.7 MB (41.67%) - LeetHub

---
+++ 1837-sum-of-digits-in-base-k/1837-sum-of-digits-in-base-k.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+var sumBase = function(n, k) {
+    let sum = 0;
+    while(n>0){
+        sum +=n%k;
+        n=Math.floor(n/k);
+    }
+    return sum;
+};
