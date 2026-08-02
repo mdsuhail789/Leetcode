51ab68b8a224227a4e2debb129389e1182794753
Muhammed. Suhail <mdsuhail63831@gmail.com>
2026-08-02 18:13:12 +0530

    Time: 1 ms (84.15%), Space: 57.2 MB (5.87%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} sentence
+ * @return {boolean}
+ */
+var checkIfPangram = function(sentence) {
+    let set = new Set();
+
+    for(let ch of sentence){
+        set.add(ch);
+    }
+     return set.size === 26;
+};
